e64948383825e2629435670b694dade1cd03ba93
godkama <steamnitz@gmail.com>
2025-01-26 15:49:46 +0100

    Version Minor 0.2.0

--- index.js
+++ index.js
@@ -5,19 +5,24 @@ const { Server } = require("socket.io");
 // init
 const app = express();
 const server = http.createServer(app);
-const io = new Server(http.createServer(app));
+const io = new Server(server);
 
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
   console.log("new user connected");
 
+  socket.on("chat message", (msg) => {
+    console.log("Message received ", msg);
+    io.emit("chat message", msg);
+  });
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 });
 
-const PORT = process.env.PORT || 727;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Access : https://localhost:${PORT}`);
