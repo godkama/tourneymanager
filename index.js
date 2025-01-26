32ca65f6032fe808d7d724cd5ef4703c34e6eea2
godkama <steamnitz@gmail.com>
2025-01-26 15:50:57 +0100

    Version Minor 0.2.0

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2727;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Access : https://localhost:${PORT}`);
